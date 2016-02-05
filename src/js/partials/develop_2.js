4cce281a20ee5ee4c8ebf84466d2835fb3fd2a48
ArtGarm <artem.garmatyk@gmail.com>
2016-02-05 18:02:23 +0200

    cfgh

--- src/js/partials/develop_2.js
+++ src/js/partials/develop_2.js
@@ -1,15 +1,17 @@
-$(document).ready(function(){
+function punishScrollPane (){
+    var hert = $(window).height() - $('.header').height() - $('.footer').height() -$('.title-text').height() - parseInt($('.wrap-one-news').css('padding-bottom')) - parseInt($('.wrap-one-news').css('padding-top')) -parseInt($('.fix-class').css('padding-top'));
 
-    var hert = $(window).height() - $('.header').height() - $('.footer').height() -$('.title-text').height() - parseInt($('.wrap-one-news').css('padding-bottom')) - parseInt($('.wrap-one-news').css('padding-top'));
+    if  (hert<250) {
+        hert = 250;
+    }
+
     $('.news-lol-scroll').css('height', hert);
 
     $('.news-lol-scroll').jScrollPane({showArrows: false, autoReinitialise:true,verticalDragMaxHeight:10, contentWidth: '0px', verticalDragMaxHeight:20});
+}
 
-
-    console.log();
-/*
-
-*/
+$(document).ready(function(){
+    punishScrollPane ();
 
 });
 
@@ -18,5 +20,5 @@ $(window).load(function(){
 });
 
 $(window).resize(function(){
-
+    punishScrollPane ();
 });
