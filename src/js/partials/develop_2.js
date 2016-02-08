4f83fb4d00973da4d48602a7c6dd2f38c92fbd8a
ArtGarm <artem.garmatyk@gmail.com>
2016-02-08 17:53:58 +0200

    fffffipd

--- src/js/partials/develop_2.js
+++ src/js/partials/develop_2.js
@@ -13,27 +13,82 @@ function punishScrollPane (){
 $(document).ready(function(){
     punishScrollPane ();
 
-    var flagShtock = true;
+    var flagShtock1 = true;
 
-    $('.img-hovered-click').click(function(){
-        if (flagShtock) {
-            $('.hide-pass').attr('type', 'text');
-            flagShtock = false ;
-            $('.img-hovered-click>img').attr('src' , 'images/in-ie8-not-belive.png' );
+    $('.img-hovered-click1').click(function(){
+        if (flagShtock1) {
+            $(this).closest('.context').find('input').attr('type', 'text');
+            $(this).find('img').attr('src' , 'images/in-ie8-not-belive.png' );
+            flagShtock1 = false ;
 
         }
         else{
-            $('.hide-pass').attr('type', 'password');
-            $('.img-hovered-click>img').attr('src' , 'images/in-ie8-belive.png');
-            flagShtock = true ;
+            $(this).closest('.context').find('input').attr('type', 'password');
+            $(this).find('img').attr('src' , 'images/in-ie8-belive.png');
+            flagShtock1 = true ;
         };
 
     });
 
-    $('.fun-move-rich-man').click(function(){
-        console.log($(this).closest('.row-sample').find('.hipe-block'));   //.fadeOut( "slow" );
-        $(this).closest('.row-sample').find('.hipe-block').css('display', 'block');
+    var flagShtock2 = true;
+
+    $('.img-hovered-click2').click(function(){
+        if (flagShtock2) {
+            $(this).closest('.context').find('input').attr('type', 'text');
+            $(this).find('img').attr('src' , 'images/in-ie8-not-belive.png' );
+            flagShtock2 = false ;
+
+        }
+        else{
+            $(this).closest('.context').find('input').attr('type', 'password');
+            $(this).find('img').attr('src' , 'images/in-ie8-belive.png');
+            flagShtock2 = true ;
+        };
+
     });
+
+
+    var redFlag = true;
+    var yelowFlag = true;
+
+    $('.olways-click-some1').click(function(){
+
+        if (redFlag){
+            redFlag = false ;
+            $(this).find('span').html('Отмена');
+            $(this).find('a').addClass('green-button');
+            $(this).closest('.big-row-convert').find('.hipe-block').stop( true, true ).fadeIn( "slow" ); //.css('display', 'block');
+        }
+        else{
+            redFlag = true ;
+            $(this).find('span').html('Изменить');
+            $(this).find('a').removeClass('green-button');
+            $(this).closest('.big-row-convert').find('.hipe-block').stop( true, true ).fadeOut("slow"); //.css('display', 'block');
+        }
+
+    });
+
+    $('.olways-click-some2').click(function(){
+        if (yelowFlag){
+            yelowFlag = false ;
+            $(this).find('span').html('Отмена');
+            $(this).find('a').addClass('green-button');
+            $(this).closest('.big-row-convert').find('.hipe-block').stop( true, true ).fadeIn( "slow" ); //.css('display', 'block');
+        }
+        else{
+            yelowFlag = true ;
+            $(this).find('span').html('Изменить');
+            $(this).find('a').removeClass('green-button');
+            $(this).closest('.big-row-convert').find('.hipe-block').stop( true, true ).fadeOut("slow"); //.css('display', 'block');
+        }
+    });
+
+    $('.subm-closest-form').click(function(){
+        $(this).closest('.hipe-block').find('form').submit();
+    })
+
+
+
 
 });
 
