270b1a225020e1843c4f06e0f6f557ebf7f480e8
ArtGarm <artem.garmatyk@gmail.com>
2016-02-08 16:49:20 +0200

    add

--- src/js/partials/develop_2.js
+++ src/js/partials/develop_2.js
@@ -13,6 +13,28 @@ function punishScrollPane (){
 $(document).ready(function(){
     punishScrollPane ();
 
+    var flagShtock = true;
+
+    $('.img-hovered-click').click(function(){
+        if (flagShtock) {
+            $('.hide-pass').attr('type', 'text');
+            flagShtock = false ;
+            $('.img-hovered-click>img').attr('src' , 'images/in-ie8-not-belive.png' );
+
+        }
+        else{
+            $('.hide-pass').attr('type', 'password');
+            $('.img-hovered-click>img').attr('src' , 'images/in-ie8-belive.png');
+            flagShtock = true ;
+        };
+
+    });
+
+    $('.fun-move-rich-man').click(function(){
+        console.log($(this).closest('.row-sample').find('.hipe-block'));   //.fadeOut( "slow" );
+        $(this).closest('.row-sample').find('.hipe-block').css('display', 'block');
+    });
+
 });
 
 $(window).load(function(){
